374bf03a372a5b7f6eda678c0e72bef0a96edc49
Yunus Akgeç <yunusakgec@gmail.com>
2024-10-13 17:42:13 +0300

    First Commit

---
+++ src/main/java/com/example/online_track_order/OnlineTrackOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.online_track_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTrackOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineTrackOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/online_track_order/OnlineTrackOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_track_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineTrackOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
